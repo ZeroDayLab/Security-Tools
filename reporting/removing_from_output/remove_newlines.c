539ca58389ee44bf459689d9b308b1926fd7e9bb
StefanMichielse <smichielse@zerodaylab.com>
2014-11-14 14:17:11 +0100

    Update remove_newlines.c

--- reporting/removing_from_output/remove_newlines.c
+++ reporting/removing_from_output/remove_newlines.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
     int k; // using int here and not char since EOF is an int
     while((k = getchar()) != EOF)
-        if(k != 0x0D && k != 0x0A) putchar(k);
-//        else putchar(''); // replace newlines with spaces
+        if(k != 0x0D && k != 0x0A) putchar(k); //0x0D is carriage return and 0x0A is new line
+//        else putchar(''); // When above is found replace character with any character
     return 0;
 }
