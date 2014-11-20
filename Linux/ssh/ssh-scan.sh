37985b9bf53b17ebfc1ca1530bdf2afff776255d
StefanMichielse <michielse@gmail.com>
2014-11-20 15:28:14 +0100

    Update ssh-scan.sh
    Added Server debug message and some minor amendments

--- Linux/ssh/ssh-scan.sh
+++ Linux/ssh/ssh-scan.sh
@@ -12,32 +12,31 @@ exit
 fi
 
 TMP=$(mktemp -d)
-echo "Directory made :"$TMP
-echo "Nohup.out is located :"$TMP"/nohup.out"
+#echo "Directory made :"$TMP
+#echo "Nohup.out is located :"$TMP"/nohup.out"
 cd $TMP
 # timeout is set to 5 seconds then it will send a SIGKILL signal to this script
 timeout -s 9 5 nohup ssh -vv $ip
-
+line="_______________"
 a=$(awk 'BEGIN {FS=",";} NR==35 { for (i=3; i<=NF;i++) print $i}' $TMP/nohup.out|awk '!/ecdh/')
 b=$(awk 'BEGIN {FS=",";} NR==36 { for (j=2; j<=NF;j++) print $j}' $TMP/nohup.out|awk '!/ecdsa/')
 c=$(awk 'BEGIN {FS=",";} NR==37 { for (k=3; k<=NF;k++) print $j}' $TMP/nohup.out|sort|awk '!/ecdh/')
 d=$(awk 'BEGIN {FS=",";} NR==39 { for (l=3; l<=NF;l++) print $k}' $TMP/nohup.out|sort)
-echo "=============================="
+echo "Server debug message" 
+echo $line
+echo $d
+echo $line
 echo "Kex Algorithms"
-echo "=============================="
-for m in $a;do echo $m;done
-echo "=============================="
-echo "Server Host Key Algorithms"
-echo "=============================="
-for n in $b;do echo $n;done
-echo "=============================="
+echo $line
+for l in $a;do echo $l;done
+echo $line
 echo "Encryption Algorithms"
-echo "=============================="
-for o in $c;do echo $o;done
-echo "=============================="
+echo $line
+for m in $b;do echo $m;done
+echo $line
 echo "MAC Algorithms"
-echo "=============================="
-for p in $d;do echo $p;done
+echo $line
+for n in $c;do echo $n;done
 #remove the temp directory
 echo "Removing directory :" $TMP
 rm $TMP
